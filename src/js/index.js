9b248e9d9d60c66776ab6bbdb1a2cc4e90844f76
Cem Ekiz <ekizcem.pro@gmail.com>
2023-03-06 17:41:16 +0100

    feat: handle placeholder on focus and focusout

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,33 @@
 if (module.hot) {
   module.hot.accept();
 }
+
+const handlePlaceholder = inputId => {
+  const input = document.getElementById(inputId);
+
+  const addPlaceholder = () => {
+    input.setAttribute('placeholder', 'Rechercher une recette');
+  };
+
+  const clearPlaceholder = () => {
+    input.setAttribute('placeholder', '');
+  };
+
+  // On focus, if the input is empty then clear the placeholder
+  input.addEventListener('focus', () => {
+    if (input.value === '') {
+      clearPlaceholder();
+    }
+  });
+
+  // On focusout, if the input is empty add the placeholder else remove it
+  input.addEventListener('focusout', () => {
+    if (input.value === '') {
+      addPlaceholder();
+    } else if (input.value !== '') {
+      input.removeAttribute('placeholder');
+    }
+  });
+};
+
+handlePlaceholder('searchbar-input');
